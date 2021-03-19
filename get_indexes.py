11f0498bae08336c15b9dc833b5d4cb5085817d0
brickj <resumerickjacobs@yahoo.com>
2021-03-18 22:29:42 -0400

    Update get_indexes.py

--- get_indexes.py
+++ get_indexes.py
@@ -6,25 +6,55 @@ list_of_nodes = ["localhost:9102"]
 dict_name_create_statement = {}
 list_of_buckets = []
 
+
+def check_create_primary():
+    global index_INDEX_start, index_ON_end, bucket
+    if not "CREATE PRIMARY INDEX" in line:
+        index_INDEX_start = "ON"
+        index_ON_end = "("
+        bucket = line[
+            line.find(index_INDEX_start)
+            + len(index_INDEX_start) : line.find(index_ON_end)
+        ].replace("`", "")
+
+
+def check_primary():
+    global index_INDEX_start, index_ON_end, bucket
+    if "INDEX `#primary`" in line:
+        index_INDEX_start = "ON"
+        index_ON_end = "("
+        bucket = line[
+            line.find(index_INDEX_start) + len(index_INDEX_start) : len(line)
+        ].replace("`", "")
+
+
 for node in list_of_nodes:
 
-    response = requests.get('http://' + node + '/getIndexStatement', auth=('admin', 'password'))
+    response = requests.get(
+        "http://" + node + "/getIndexStatement", auth=("admin", "password")
+    )
 
-    path = '/Users/rickjacobs/Downloads/indexes.txt'
+    path = "/Users/rickjacobs/Downloads/indexes.txt"
     text_file = open(path, "w")
+
+    print(response.json())
 
     for i in range(len(response.json())):
         line = response.json()[i]
 
+        print(line)
+
         index_INDEX_start = "INDEX"
         index_ON_end = "ON"
 
-        index_name = line[line.find(index_INDEX_start) + len(index_INDEX_start):line.find(index_ON_end)].replace("`",
-                                                                                                                 "")
-        index_INDEX_start = "ON"
-        index_ON_end = "("
-        bucket = line[line.find(index_INDEX_start) + len(index_INDEX_start):line.find(index_ON_end)].replace(
-            "`", "")
+        index_name = line[
+            line.find(index_INDEX_start)
+            + len(index_INDEX_start) : line.find(index_ON_end)
+        ].replace("`", "")
+
+        check_create_primary()
+
+        check_primary()
 
         if not bucket in list_of_buckets:
             list_of_buckets.append(bucket)
@@ -38,10 +68,17 @@ for node in list_of_nodes:
 
     for bucket in list_of_buckets:
         bucket = bucket.strip()
-        dict_name_create_statement[
-            bucket] = "BUILD INDEX ON '" + bucket + "' ((SELECT RAW name FROM system:indexes WHERE keyspace_id = '" + bucket + "' AND state = 'deferred'))"
+
+        print(bucket)
+        dict_name_create_statement[bucket] = (
+            "BUILD INDEX ON `"
+            + bucket
+            + "` ((SELECT RAW name FROM system:indexes WHERE keyspace_id = `"
+            + bucket
+            + "` AND state = 'deferred'))"
+        )
 
     for value in dict_name_create_statement.values():
-        text_file.write(value + ';\n')
+        text_file.write(value + ";\n")
 
 text_file.close()
